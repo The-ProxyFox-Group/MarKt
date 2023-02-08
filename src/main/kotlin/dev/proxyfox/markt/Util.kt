20ff2a5bc4d3427bd8a8f1c437f02dcf68bcc705
Octal <59418903+oliver-makes-code@users.noreply.github.com>
2023-02-08 01:27:55 -0600

    Replace reduceBy with sumOf

--- src/main/kotlin/dev/proxyfox/markt/Util.kt
+++ src/main/kotlin/dev/proxyfox/markt/Util.kt
@@ -1,6 +1,5 @@
 package dev.proxyfox.markt
 
-public inline fun <reified T> Collection<T>.reduceBy(crossinline value: T.() -> Int): Int = map(value).reduce { a, b -> a + b }
 public inline fun <reified T, reified O> Collection<T>.firstNonNull(crossinline value: T.() -> O?): Pair<T, O>? {
     for (t in this) {
         return t to (t.value() ?: continue)
